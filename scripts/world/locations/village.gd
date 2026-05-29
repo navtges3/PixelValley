4669516063d054a28ac2d91a5004de2741ea6149
Nick Avtges <navtges3@gmail.com>
2026-05-29 10:20:34 -0400

    feat: Implement building system with entrance zones and connection to player location

--- scripts/world/locations/village.gd
+++ scripts/world/locations/village.gd
@@ -3,3 +3,11 @@ class_name VillageLocation
 
 func _get_screen_name() -> ScreenManager.ScreenName:
 	return ScreenManager.ScreenName.VILLAGE
+
+func _on_location_ready() -> void:
+	for building in get_tree().get_nodes_in_group("building"):
+		(building as Building).building_entered.connect(_on_building_entered)
+
+func _on_building_entered(building: Building) -> void:
+	GameState.set_player_location(ScreenManager.ScreenName.VILLAGE, building.entrance_id)
+	ScreenManager.go_to_screen(building.screen_target)
