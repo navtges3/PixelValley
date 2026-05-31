101df0df8b7c3de4ebecee4656725c1c07d64ee3
Nick Avtges <navtges3@gmail.com>
2026-05-31 15:00:47 -0400

    feat: Add new building interiors and update scene paths for inn, potion shop, and weapon shop

---
+++ scripts/world/buildings/interior/potion_shop_interior.gd
@@ -0,0 +1,22 @@
+extends BaseLocation
+class_name PotionShopInterior
+
+const SCREEN_NAME := ScreenManager.ScreenName.POTION_SHOP
+
+@onready var shop_window: ShopWindow = $ShopWindow
+@onready var interact_area: InteractArea = $InteractArea
+
+func _get_screen_name() -> ScreenManager.ScreenName:
+	return SCREEN_NAME
+
+func _on_location_ready() -> void:
+	interact_area.interacted.connect(_on_counter_interacted)
+	shop_window.closed.connect(_on_window_closed)
+	shop_window.hide()
+
+func _on_counter_interacted() -> void:
+	player.movement_blocked = true
+	shop_window.open(ShopWindow.ShopType.POTION)
+
+func _on_window_closed() -> void:
+	player.movement_blocked = false
