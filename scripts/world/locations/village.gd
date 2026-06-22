50481bc8c4569d21cdf93734e7336d67803fbb2c
Nick Avtges <navtges3@gmail.com>
2026-06-22 15:38:46 -0400

    Add quest window UI and functionality
    - Created a new QuestWindow scene and script to manage quest interactions.
    - Implemented buttons for available and completed quests, along with a scrollable quest list.
    - Integrated reward window for quest completion rewards.
    - Updated village location to include quest board interaction, opening the quest window.
    - Adjusted screen targets for various locations to improve navigation.
    - Removed unused quest_screen script and associated UID.
    - Refactored existing scripts to connect with the new quest window functionality.

--- scripts/world/locations/village.gd
+++ scripts/world/locations/village.gd
@@ -2,17 +2,27 @@ extends BaseLocation
 class_name VillageLocation
 
 @onready var exit_trigger_zone: TriggerZone = $TriggerZones/ExitTriggerZone
-@onready var quest_trigger_zone: TriggerZone = $TriggerZones/QuestTriggerZone
+@onready var quest_interact_area: InteractArea = $Props/QuestBoard/InteractArea
+@onready var quest_window: QuestWindow = $CanvasLayer/QuestWindow
 
 func _get_screen_name() -> ScreenManager.ScreenName:
 	return ScreenManager.ScreenName.VILLAGE
 
 func _on_location_ready() -> void:
 	exit_trigger_zone.screen_target = ScreenManager.ScreenName.VALLEY
-	quest_trigger_zone.screen_target = ScreenManager.ScreenName.QUEST
+	quest_interact_area.interacted.connect(_on_quest_board_interacted)
+	quest_window.closed.connect(_on_window_closed)
+	quest_window.hide()
 	for building in get_tree().get_nodes_in_group("building"):
 		(building as Building).building_entered.connect(_on_building_entered)
 
 func _on_building_entered(building: Building) -> void:
 	GameState.set_player_location(ScreenManager.ScreenName.VILLAGE, building.entrance_id)
 	ScreenManager.go_to_screen(building.screen_target, building.entrance_id)
+
+func _on_quest_board_interacted() -> void:
+	player.movement_blocked = true
+	quest_window.open()
+
+func _on_window_closed() -> void:
+	player.movement_blocked = false
