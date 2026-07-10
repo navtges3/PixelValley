e76d0d3892aaa90a4f25bd7384f30b827e6892bc
Nick Avtges <navtges3@gmail.com>
2026-07-09 20:04:10 -0400

    Add new effects: Energize, Focus, Fortify, Healing, Invigorate, Power, Regeneration, Warding; update potions to utilize new effects

--- scripts/data/effect_stat_change.gd
+++ scripts/data/effect_stat_change.gd
@@ -6,9 +6,8 @@ class_name EffectStatChange
 @export var operation: Effect.EffectOperation = Effect.EffectOperation.ADD
 @export var base_amount: int = 0
 @export var amount_per_level: int = 0
-@export var scales_with_level: bool = false
 
 func get_amount(level: int) -> int:
-	if not scales_with_level:
-		return base_amount
-	return base_amount + ((level - 1) * amount_per_level)
+	if amount_per_level > 0:
+		return base_amount + ((level - 1) * amount_per_level)
+	return base_amount
