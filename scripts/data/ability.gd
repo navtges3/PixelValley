d2898a8af43b2514dbec31906d29a03362b46620
Nick Avtges <navtges3@gmail.com>
2026-07-10 15:42:46 -0400

    Refactor potion and weapon resources to organize effects into buffs and debuffs
    - Updated paths for potion effects to categorize them under 'buffs' for focus, regeneration, warding, and fortify potions.
    - Added new effects for knight weapons, including buffs for power and fortify.
    - Enhanced ability script to include caster and target effects, allowing for more complex interactions during ability usage.
    - Improved string representation of abilities to include caster and target effects for better clarity in combat logs.

--- scripts/data/ability.gd
+++ scripts/data/ability.gd
@@ -6,6 +6,8 @@ class_name Ability
 @export var cooldown: int
 @export var attack: Attack = null
 @export var utilities: Array[Utility] = []
+@export var caster_effects: Array[Effect] = []
+@export var target_effects: Array[Effect] = []
 @export var condition: Condition = null
 
 var current_cooldown: int
@@ -22,8 +24,10 @@ func use(caster: Combatant, target: Combatant) -> String:
 	if is_ready(caster, target) and caster.current_nrg >= self.energy_cost:
 		if attack != null:
 			output += attack.apply_attack(caster, target)
-		for utility in utilities:
-			output += utility.apply_utility(caster, target)
+		for effect: Effect in caster_effects:
+			output += caster.apply_effect(effect.duplicate())
+		for effect: Effect in target_effects:
+			output += target.apply_effect(effect.duplicate())
 		self.current_cooldown = self.cooldown + 1
 		caster.current_nrg -= self.energy_cost
 		return output
@@ -36,9 +40,17 @@ func update_cooldown() -> void:
 
 func _to_string(combatant: Combatant = null) -> String:
 	var attack_string := ""
-	var utility_string := ""
+	var effects_string := ""
 	if attack != null:
 		attack_string = attack._to_string(combatant) + "\n"
-	for utility in utilities:
-		utility_string += "%s\n" % utility._to_string()
-	return "%s%s(Energy cost: %d, Cooldown: %d)" % [attack_string, utility_string, self.energy_cost, self.cooldown]
+	if not caster_effects.is_empty():
+		effects_string += "Self:"
+		for effect: Effect in caster_effects:
+			effects_string += "\n -%s" % effect._to_string()
+		effects_string += "\n"
+	if not target_effects.is_empty():
+		effects_string += "Target:"
+		for effect: Effect in target_effects:
+			effects_string += "\n -%s" % effect._to_string()
+		effects_string += "\n"
+	return "%s%s(Energy cost: %d, Cooldown: %d)" % [attack_string, effects_string, self.energy_cost, self.cooldown]
