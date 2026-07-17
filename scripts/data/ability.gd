a7e5c0e0406094e2a035232f2a74585594b2fd6a
Nick Avtges <navtges3@gmail.com>
2026-07-17 12:46:52 -0400

    Pass the dispatcher through Ability

--- scripts/data/ability.gd
+++ scripts/data/ability.gd
@@ -18,18 +18,18 @@ func is_ready(caster: Combatant = null, target: Combatant = null) -> bool:
 		return condition.check(caster, target)
 	return true
 
-func use(caster: Combatant, target: Combatant) -> String:
+func use(caster: Combatant, target: Combatant, effect_dispatcher: EffectEventDispatcher = null) -> String:
 	var output := "%s used %s!\n" % [caster.get_colored_name(), self.name]
 	if is_ready(caster, target) and caster.current_nrg >= self.energy_cost:
 		if attack != null:
 			output += attack.apply_attack(caster, target)
 		for effect: Effect in caster_effects:
 			var effect_copy: Effect = effect.duplicate() as Effect
-			var result: EffectManager.ApplicationResult = EffectManager.apply_effect(effect_copy, caster, caster)
+			var result: EffectManager.ApplicationResult = EffectManager.apply_effect(effect_copy, caster, caster, 0, effect_dispatcher)
 			output += result.output
 		for effect: Effect in target_effects:
 			var effect_copy: Effect = effect.duplicate() as Effect
-			var result: EffectManager.ApplicationResult = EffectManager.apply_effect(effect_copy, caster, target)
+			var result: EffectManager.ApplicationResult = EffectManager.apply_effect(effect_copy, caster, target, 0, effect_dispatcher)
 			output += result.output
 		self.current_cooldown = self.cooldown + 1
 		caster.current_nrg -= self.energy_cost
