c7020e7c4d23045576e33dfed91a1de5ba6eb1ce
Nick Avtges <46537996+navtges3@users.noreply.github.com>
2026-07-17 06:14:01 -0400

    Add a read-only effect snapshot and replace the public active-effect query

---
+++ scripts/data/effect_view.gd
@@ -0,0 +1,38 @@
+extends RefCounted
+class_name EffectView
+
+var effect_id: StringName:
+	get:
+		return _effect_id
+
+var display_name: String:
+	get:
+		return _display_name
+
+var level: int:
+	get:
+		return _level
+
+var remaining_turns: int:
+	get:
+		return _remaining_turns
+
+var persistence: Effect.Persistence:
+	get:
+		return _persistence
+
+var _effect_id: StringName
+var _display_name: String
+var _level: int
+var _remaining_turns: int
+var _persistence: Effect.Persistence
+
+func _init(active_effect: ActiveEffect) -> void:
+	assert(active_effect != null)
+	assert(active_effect.effect != null)
+	
+	_effect_id = active_effect.effect.effect_id
+	_display_name = active_effect.effect.effect_name
+	_level = active_effect.effect.level
+	_remaining_turns = active_effect.remaining_turns
+	_persistence = active_effect.effect.persistence
