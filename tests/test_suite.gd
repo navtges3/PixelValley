5642961a4d9f2749c631c031428b811efb39a8b9
Nick Avtges <navtges3@gmail.com>
2026-07-20 18:52:39 -0400

    Reworked rewards
    Added RewardSystem to handle rewards.

--- tests/test_suite.gd
+++ tests/test_suite.gd
@@ -13,11 +13,16 @@ extends Node
 	$Quests/QuestSaveMigrationTests,
 ]
 
+@onready var _reward_test_cases: Array[TestCase] = [
+	$Rewards/RewardServiceTests,
+]
+
 
 func _ready() -> void:
 	var total_failures: int = 0
 	total_failures += _run_section("EFFECTS", _effect_test_cases)
 	total_failures += _run_section("QUESTS", _quest_test_cases)
+	total_failures += _run_section("REWARDS", _reward_test_cases)
 
 	print("\n========== TEST SUMMARY ==========")
 	if total_failures == 0:
