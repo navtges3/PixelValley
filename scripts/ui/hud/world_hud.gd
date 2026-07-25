a3d1363d7b67f4c19f24677db2f06dd62cda8f54
Nick Avtges <navtges3@gmail.com>
2026-07-25 16:55:50 -0400

    Compose DialogueWindow and DialogueRunner into WorldHUD

--- scripts/ui/hud/world_hud.gd
+++ scripts/ui/hud/world_hud.gd
@@ -1,9 +1,28 @@
 extends CanvasLayer
 class_name WorldHUD
 
+signal dialogue_opened
+signal dialogue_closed(reason: DialogueRunner.FinishReason)
+signal dialogue_action_requested(action: DialogueAction, context: Dictionary[StringName, Variant])
+
 @onready var game_hud: GameHUD = $GameHud
 @onready var hero_hud: HeroHUD = $HeroHUD
 @onready var tracked_quest_hud: TrackedQuestHUD = $TrackedQuestHUD
+
+@onready var dialogue_window: DialogueWindow = $DialogueLayer/DialogueWindow
+var dialogue_runner: DialogueRunner
+
+func _ready() -> void:
+	dialogue_runner = DialogueRunner.new()
+	dialogue_runner.conversation_started.connect(_on_conversation_started)
+	dialogue_runner.line_changed.connect(dialogue_window.show_line)
+	dialogue_runner.responses_changed.connect(dialogue_window.show_responses)
+	dialogue_runner.action_requested.connect(dialogue_action_requested.emit)
+	dialogue_runner.conversation_finished.connect(_on_conversation_finished)
+	dialogue_window.advance_requested.connect(dialogue_runner.advance)
+	dialogue_window.response_requested.connect(dialogue_runner.choose_response)
+	dialogue_window.cancel_requested.connect(dialogue_runner.cancel)
+	dialogue_window.close()
 
 func hide_all() -> void:
 	hide()
@@ -27,3 +46,19 @@ func close_game_hud() -> void:
 
 func set_hero_hud_visible(vis: bool) -> void:
 	hero_hud.visible = vis
+
+func start_dialogue(conversation: DialogueConversation, context: Dictionary[StringName, Variant] = {}) -> bool:
+	if game_hud.is_open() or dialogue_runner.is_running():
+		return false
+	return dialogue_runner.start(conversation, context)
+
+func is_dialogue_open() -> bool:
+	return dialogue_runner.is_running()
+
+func _on_conversation_started(_conversation: DialogueConversation) -> void:
+	dialogue_window.open()
+	dialogue_opened.emit()
+
+func _on_conversation_finished(reason: DialogueRunner.FinishReason) -> void:
+	dialogue_window.close()
+	dialogue_closed.emit(reason)
