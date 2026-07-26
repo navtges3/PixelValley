14e70dfc39f43f50279478fddf8ecd0f862ebf18
Nick Avtges <navtges3@gmail.com>
2026-07-26 10:29:09 -0400

    Make InteractArea safely disableable

--- scripts/world/interact_area.gd
+++ scripts/world/interact_area.gd
@@ -7,10 +7,23 @@ signal interacted
 
 var _player_inside: bool = false
 var _player: Player = null
+var _enabled: bool = true
+
+func set_enabled(enabled: bool) -> void:
+	if _enabled == enabled:
+		return
+	_enabled = enabled
+	set_process_unhandled_input(_enabled)
+	monitoring = _enabled
+	if not _enabled and _player != null:
+		_player.clear_prompt(prompt_text)
+		_player_inside = false
+		_player = null
 
 func _unhandled_input(event: InputEvent) -> void:
-	if (not _player_inside or not event.is_action_pressed("interact") or
-		_player == null or _player.movement_blocked):
+	if (not _enabled or not _player_inside
+		or not event.is_action_pressed("interact")
+		or _player == null or _player.movement_blocked):
 		return
 	get_viewport().set_input_as_handled()
 	interacted.emit()
