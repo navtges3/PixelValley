ca3cd7b99a362c57f0c7f0dfcfaf0e967e51fb53
Nick Avtges <46537996+navtges3@users.noreply.github.com>
2026-09-25 07:24:03 -0400

    Warning fix

--- scripts/system_managers/battle_manager.gd
+++ scripts/system_managers/battle_manager.gd
@@ -374,8 +374,8 @@ func _grant_victory_rewards() -> Array[RewardEntry]:
 		if gold != null:
 			entries.append(gold)
 		entries.append_array(RewardService.grant_loot(enemy.roll_loot(), reward_party))
-	for hero: Hero in recipients:
-		var xp_entry := RewardService.grant_experience(hero, total_experience)
+	for member: Hero in recipients:
+		var xp_entry := RewardService.grant_experience(member, total_experience)
 		if xp_entry != null:
 			entries.append(xp_entry)
 	return entries
